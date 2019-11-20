4285e7b3445aed9d907eee17bbb3e11f4041d03e
tomogo <tomas.janovsky@gmail.com>
2019-11-20 14:21:22 +0100

    Added class LiquidCrystalNationalCharsets.

--- LiquidCrystalNationalCharsets.cpp
+++ LiquidCrystalNationalCharsets.cpp
@@ -1,41 +1,74 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <LiquidCrystalNationalCharsets.h>
 
-uint8_t insteadOff[8] = {255, 255, 255, 255, 255, 255, 255, 255};
+LiquidCrystalNationalCharsets::LiquidCrystalNationalCharsets(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3):LiquidCrystal( rs, enable, d0, d1, d2, d3) {
+}
 
-void setCursorLCD(LiquidCrystal &lcd, int column, int row) {
+void LiquidCrystalNationalCharsets::begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS) {
+  LiquidCrystal::begin(cols, rows);
+  _cols=cols;
+  _rows=rows;
+}
+
+void LiquidCrystalNationalCharsets::setCursorLCD(int column, int row) {
   if ( column >= 0 && row >= 0 ) {
-    lcd.setCursor(column, row);
+    LiquidCrystal::setCursor(column, row);
   }
 }
 
-void createChar(LiquidCrystal &lcd, uint8_t location, uint8_t charmap[], uint8_t instead) {
-  lcd.createChar(location, charmap);
-  insteadOff[location]=instead;
+void LiquidCrystalNationalCharsets::createChar(uint8_t location, uint8_t charmap[], uint8_t instead){
+  LiquidCrystal::createChar(location, charmap);
+  insteadOff[location]=instead;  
 }
 
-void writeLCD(LiquidCrystal &lcd, uint8_t outChar, int column, int row) {
-  setCursorLCD(lcd, column, row);
-
-  for (uint8_t i=0; i<8; i++) {
-    if (outChar==insteadOff[i]) outChar=uint8_t(i);
-  }
-
-  lcd.write(outChar);
-}
-
-void writeLCD(LiquidCrystal &lcd, String &text, int column, int row) {
-  setCursorLCD(lcd, column, row);
+void LiquidCrystalNationalCharsets::writeLCD( String text, int column, int row) {
+  setCursorLCD( column, row);
   
   for(int i=0; i<text.length(); i++) {
-    writeLCD( lcd, text.charAt(i), -1, -1);
+    writeLCD( text.charAt(i), -1, -1);
   }
 }
 
-void writeLCD(LiquidCrystal &lcd, uint8_t *text, int column, int row) {
-  setCursorLCD(lcd, column, row);
-  
-  for (int i=0; text[i] != 0; i++) {
-    writeLCD( lcd, text[i], -1, -1);
+void LiquidCrystalNationalCharsets::writeLCD( uint8_t outChar, int column, int row) {
+  setCursorLCD( column, row);
+
+  for (uint8_t i=0; i<8; i++) {
+    if (outChar==insteadOff[i]) {
+      outChar=uint8_t(i);
+      i=8;
+    }
   }
+  write(outChar);
+}
+
+void LiquidCrystalNationalCharsets::writeLCD( uint8_t *text, int column, int row) {
+  setCursorLCD(column, row);
+  
+  for (int i=0; *text != 0; i++) {
+    writeLCD( *text++, -1, -1);
+  }
+}
+
+void LiquidCrystalNationalCharsets::scrollStringLeft(String r, uint8_t row) {
+  String r1;
+  String r2;
+  for (uint8_t i = 0; i < _cols; i++) {
+    r1 += " ";
+  }
+  writeLCD(r1, 0, row);
+  r1 += r;
+  if (scrollIndex+_cols>r1.length()){
+    r2=r1.substring(scrollIndex,r1.length());
+  } else {
+    r2=r1.substring(scrollIndex,scrollIndex+_cols);
+  }
+  delay(25);
+  writeLCD(r2, 0, row);
+  delay(350);
+  if (scrollIndex++>=r1.length()) scrollIndex=0;
+}
+
+void LiquidCrystalNationalCharsets::scrollReset() {
+  scrollIndex=0;
 }
--- LiquidCrystalNationalCharsets.h
+++ LiquidCrystalNationalCharsets.h
@@ -1,203 +1,88 @@
-/*
-Czech charset for LCD display. There are only czech small chars: áčďéěíňóřšťúůýž
-you can add next chars you need for your language
+/*National charset for LiquidCrystal.h
+czech.h contains only czech small chars: áčďéěíňóřšťúůýž
+you can add next chars by your self in next lang.h files
 
-There are named [long/hook/ring/umlaut]_[char]
+Characters are named [long/hook/ring/umlaut]_[char]
 
-Using:
+Using in Arduino scetch:
 
-#include <LiquidCrystal.h>
 #include <LiquidCrystalNationalCharsets.h>
+#include <czech.h>
 
-const int RS = 8;
-const int EN = 9;
-const int d4 = 4;
-const int d5 = 5;
-const int d6 = 6;
-const int d7 = 7;
+// Wires for LCD
+const int RS = 11;
+const int RW = 13;
+const int EN = 10;
+const int d4 = 5;
+const int d5 = 4;
+const int d6 = 3;
+const int d7 = 2;
 
-LiquidCrystal lcd( RS, EN, d4, d5, d6, d7);
+
+LiquidCrystalNationalCharsets lcd(RS,  EN,  d4,  d5,  d6,  d7);
 
 void setup() {
-// You can add chars with id 0-7 in procedure: createChar(yourLCD, id, charMap, charInsteadOf)
+  // You can define chars with id 0-7 in procedure: createChar(yourLCD, id, charMap, charInsteadOf)
+  
+  lcd.createChar(0, hook_c, '#');
+  lcd.createChar(1, hook_e, '$');
+  lcd.createChar(2, hook_s, '%');
+  lcd.createChar(3, long_i, '^');
+  lcd.createChar(4, hook_d, '&');
+  lcd.createChar(5, hook_z, '|');
+  lcd.createChar(6, long_u, '\'');
+  lcd.createChar(7, ring_u, '+');
+  
+  lcd.begin(16,2);
+  
+  // Print on LCD by procedure: writeLCD(yourLCD, char/String, column, row)
+  // This prints "Test: čěšíďžúů" on the cursor position: 
+  lcd.writeLCD("Test: #$%^&|'+", -1, -1);
+  
+  //This prints "ě" on position 15,0 
+  lcd.writeLCD('$', 15, 0);
 
-createChar(lcd, 0, hook_c, '#');
-createChar(lcd, 1, hook_e, '$');
-createChar(lcd, 2, hook_s, '%');
-createChar(lcd, 3, long_i, '^');
-createChar(lcd, 4, hook_d, '&');
-createChar(lcd, 5, hook_z, '|');
-createChar(lcd, 6, long_u, ''');
-createChar(lcd, 7, ring_u, '+');
+  // This stes cursor on line 1 first position:
+  lcd.setCursor(0,1);
 
-lcd.begin(16,2);
+  //This prints "Hi!" on cursor position:
+  lcd.writeLCD("Hi!", -1, -1);
 
-// Print on LCD by procedure: writeLCD(yourLCD, char/String, column, row)
-// This prints "Test: ěšíďžúů" on the cursor position: 
-writeLCD(lcd, "Test: #$%^&|'+", -1, -1);
+  String hallo=", hallo";
+  lcd.writeLCD(hallo, -1, -1);
+  delay(3000);
+}
 
-//This prints "ě" on row 1 
-writeLCD(lcd, '#', 0, 1);
-
-//This prints "Hi!" on cursor position:
-writeLCD(lcd, "Hi!", -1, -1);
+void loop() {
+  lcd.scrollStringLeft("Hi bro!",1);
 }
 */
-
 
 #ifndef LiquidCrystalNationalCharsets_h
 #define LiquidCrystalNationalCharsets_h
 
-void setCursorLCD(LiquidCrystal &lcd, int column, int row);
-void createChar(LiquidCrystal &lcd, uint8_t location, uint8_t charmap[], uint8_t instead);
-void printLCD(LiquidCrystal &lcd, uint8_t outChar, int column, int row);
-void writeLCD(LiquidCrystal &lcd, String &text, int column, int row);
-void writeLCD(LiquidCrystal &lcd, uint8_t *text, int column, int row);
+#include <LiquidCrystal.h>
 
-// Czech small capitals
-uint8_t smiley[8] = {
-  B00000,
-  B10001,
-  B00000,
-  B00000,
-  B10001,
-  B01110,
-  B00000,
-};
-uint8_t long_a[8] = {
-  B00010,
-  B00100,
-  B01110,
-  B00001,
-  B01111,
-  B10001,
-  B01111,
-};
-uint8_t hook_c[8] = {
-  B01010,
-  B00100,
-  B01110,
-  B10000,
-  B10000,
-  B10001,
-  B01110,
-};
-uint8_t hook_d[8] = {
-  B00101,
-  B00011,
-  B01101,
-  B10011,
-  B10001,
-  B10001,
-  B01110,
-};
-uint8_t hook_e[8] = {
-  B01010,
-  B00100,
-  B01110,
-  B10001,
-  B11111,
-  B10000,
-  B01110,
-};
-uint8_t long_e[8] = {
-  B00010,
-  B00100,
-  B01110,
-  B10001,
-  B11111,
-  B10000,
-  B01110,
-};
-uint8_t long_i[8] = {
-  B00010,
-  B00100,
-  B01100,
-  B00100,
-  B00100,
-  B00100,
-  B01110,
-};
-uint8_t hook_n[8] = {
-  B01010,
-  B00100,
-  B10110,
-  B11001,
-  B10001,
-  B10001,
-  B10001,
-};
-uint8_t long_o[8] = {
-  B00010,
-  B00100,
-  B01110,
-  B10001,
-  B10001,
-  B10001,
-  B01110,
-};
-uint8_t hook_r[8] = {
-  B01010,
-  B00100,
-  B10110,
-  B11001,
-  B10000,
-  B10000,
-  B10000,
-};
-uint8_t hook_s[8] = {
-  B01010,
-  B00100,
-  B01110,
-  B10000,
-  B01110,
-  B00001,
-  B11110,
-};
-uint8_t hook_t[8] = {
-  B01101,
-  B01010,
-  B11100,
-  B01000,
-  B01000,
-  B01001,
-  B00110,
-};
-uint8_t long_u[8] = {
-  B00010,
-  B00100,
-  B10001,
-  B10001,
-  B10001,
-  B10011,
-  B01101,
-};
-uint8_t ring_u[8] = {
-  B00110,
-  B00110,
-  B10001,
-  B10001,
-  B10001,
-  B10011,
-  B01101,
-};
-uint8_t long_y[8] = {
-  B00010,
-  B00100,
-  B10001,
-  B10001,
-  B01111,
-  B00001,
-  B01110,
-};
-uint8_t hook_z[8] = {
-  B01010,
-  B00100,
-  B11111,
-  B00010,
-  B00100,
-  B01000,
-  B11111,
+class LiquidCrystalNationalCharsets : public LiquidCrystal {
+  public:
+    LiquidCrystalNationalCharsets(uint8_t rs, uint8_t enable, uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3);
+    
+    void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+
+    void setCursorLCD(int column, int row);
+    void createChar(uint8_t location, uint8_t charmap[], uint8_t instead);
+    
+    void writeLCD(String text, int column, int row);
+    void writeLCD(uint8_t outChar, int column, int row);
+    void writeLCD(uint8_t *text, int column, int row);
+    
+    void scrollStringLeft(String r, uint8_t row);
+    void scrollReset();
+  private:
+    uint8_t insteadOff[8] = {255, 255, 255, 255, 255, 255, 255, 255};
+    uint8_t _cols;
+    uint8_t _rows;
+    int scrollIndex=0;
 };
 
 #endif
